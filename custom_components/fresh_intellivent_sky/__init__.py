387862937c4f14349bdb0acb39a4fe4a35c5837f
Ståle Storø Hauknes <stale.semb@airthings.com>
2022-11-27 02:11:13 +0100

    Fix linting issues, fix connection issues

--- custom_components/fresh_intellivent_sky/__init__.py
+++ custom_components/fresh_intellivent_sky/__init__.py
@@ -1,33 +1,24 @@
 """The Fresh Intellivent Sky integration."""
 from __future__ import annotations
 
-from datetime import timedelta
 import logging
-
-from pyfreshintellivent import FreshIntelliVent
+from datetime import timedelta
 
 from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady, HomeAssistantError
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.update_coordinator import (DataUpdateCoordinator,
+                                                      UpdateFailed)
+from pyfreshintellivent import (FreshIntelliVent,
+                                FreshIntelliventError,
+                                FreshIntelliventTimeoutError)
 
-from .const import (
-    AIRING_MODE_UPDATE,
-    CONF_AUTH_KEY,
-    CONSTANT_SPEED_UPDATE,
-    DEFAULT_SCAN_INTERVAL,
-    DELAY_KEY,
-    DETECTION_KEY,
-    DOMAIN,
-    ENABLED_KEY,
-    HUMIDITY_MODE_UPDATE,
-    LIGHT_AND_VOC_MODE_UPDATE,
-    MINUTES_KEY,
-    RPM_KEY,
-    TIMER_MODE_UPDATE,
-)
+from .const import (AIRING_MODE_UPDATE, CONF_AUTH_KEY, CONSTANT_SPEED_UPDATE,
+                    DEFAULT_SCAN_INTERVAL, DOMAIN, HUMIDITY_MODE_UPDATE,
+                    LIGHT_AND_VOC_MODE_UPDATE, TIMER_MODE_UPDATE)
+from .fetch_and_update import FetchAndUpdate
 
 
 class UnableToConnect(HomeAssistantError):
@@ -77,7 +68,7 @@ async def async_setup_entry(
             f"Could not find Fresh Intellivent Sky device with address {address}"
         )
 
-    async def _async_update_method():  # pylint: disable=too-many-branches, too-many-statements
+    async def _async_update_method():
         """Get data from Fresh Intellivent Sky."""
         ble_device = bluetooth.async_ble_device_from_address(hass, address)
         fresh = FreshIntelliVent()
@@ -89,104 +80,15 @@ async def async_setup_entry(
                 await client.fetch_sensor_data()
                 await client.fetch_device_information()
 
-                if auth_key is not None:
-                    await client.fetch_airing()
-                    await client.fetch_constant_speed()
-                    await client.fetch_humidity()
-                    await client.fetch_light_and_voc()
-                    await client.fetch_timer()
-                else:
-                    return fresh
-
-                updated = False
-
                 try:
-                    humidity_mode = hass.data[HUMIDITY_MODE_UPDATE]
-
-                    if humidity_mode is not None:
-                        await client.update_humidity(
-                            enabled=bool(humidity_mode[ENABLED_KEY]),
-                            detection=humidity_mode[DETECTION_KEY],
-                            rpm=int(humidity_mode[RPM_KEY]),
-                        )
-                        updated = True
-                        _LOGGER.debug("Updated humidity mode")
-
-                except Exception as err:  # pylint: disable=broad-except
-                    _LOGGER.warning("Failed to update humidity: %s", err)
-                finally:
-                    hass.data[HUMIDITY_MODE_UPDATE] = None
-
-                try:
-                    light_and_voc_mode = hass.data[LIGHT_AND_VOC_MODE_UPDATE]
-
-                    if light_and_voc_mode is not None:
-                        light = light_and_voc_mode["light"]
-                        voc = light_and_voc_mode["voc"]
-
-                        await client.update_light_and_voc(
-                            light_enabled=bool(light[ENABLED_KEY]),
-                            light_detection=light[DETECTION_KEY],
-                            voc_enabled=bool(voc[ENABLED_KEY]),
-                            voc_detection=voc[DETECTION_KEY],
-                        )
-                        updated = True
-                        _LOGGER.debug("Updated light and voc mode")
-                except Exception as err:  # pylint: disable=broad-except
-                    _LOGGER.warning("Failed to update light and voc mode: %s", err)
-                finally:
-                    hass.data[LIGHT_AND_VOC_MODE_UPDATE] = None
-
-                try:
-                    airing_mode = hass.data[AIRING_MODE_UPDATE]
-
-                    if airing_mode is not None:
-                        await client.update_airing(
-                            enabled=bool(airing_mode[ENABLED_KEY]),
-                            minutes=airing_mode[MINUTES_KEY],
-                            rpm=airing_mode[RPM_KEY],
-                        )
-                        updated = True
-                        _LOGGER.debug("Updated airing mode")
-                except Exception as err:  # pylint: disable=broad-except
-                    _LOGGER.warning("Failed to update airing mode: %s", err)
-                finally:
-                    hass.data[AIRING_MODE_UPDATE] = None
-
-                try:
-                    constant_speed = hass.data[CONSTANT_SPEED_UPDATE]
-
-                    if constant_speed is not None:
-                        await client.update_constant_speed(
-                            enabled=constant_speed[ENABLED_KEY],
-                            rpm=constant_speed[RPM_KEY],
-                        )
-                        updated = True
-                        _LOGGER.debug("Updated constant speed")
-                except Exception as err:  # pylint: disable=broad-except
-                    _LOGGER.warning("Failed to update constant speed: %s", err)
-                finally:
-                    hass.data[CONSTANT_SPEED_UPDATE] = None
-
-                try:
-                    timer_mode = hass.data[TIMER_MODE_UPDATE]
-
-                    if timer_mode is not None:
-                        await client.update_timer(
-                            minutes=timer_mode[MINUTES_KEY],
-                            delay_enabled=timer_mode[DELAY_KEY][ENABLED_KEY],
-                            delay_minutes=timer_mode[DELAY_KEY][MINUTES_KEY],
-                            rpm=timer_mode[RPM_KEY],
-                        )
-                        updated = True
-                        _LOGGER.debug("Updated timer mode")
-                except Exception as err:  # pylint: disable=broad-except
-                    _LOGGER.warning("Failed to update timer mode: %s", err)
-                finally:
-                    hass.data[TIMER_MODE_UPDATE] = None
-
-                if updated:
-                    await client.fetch_sensor_data()
+                    updates = FetchAndUpdate(hass=hass, client=client)
+                    updates.update_all()
+                except FreshIntelliventTimeoutError as err:
+                    # No important data here, just ignore and log it
+                    _LOGGER.warning("Timeout while fetch/update modes: %s", err)
+        
+                except FreshIntelliventError as err:
+                    _LOGGER.warning("Error while fetch/update modes: %s", err)
 
         except Exception as err:  # pylint: disable=broad-except
             _LOGGER.warning("Failed to fetch data: %s", err)
---
+++ custom_components/fresh_intellivent_sky/fetch_and_update.py
@@ -0,0 +1,136 @@
+from homeassistant.core import HomeAssistant
+from pyfreshintellivent import FreshIntelliVent
+import logging
+from .const import (
+    AIRING_MODE_UPDATE,
+    BOOST_UPDATE,
+    CONSTANT_SPEED_UPDATE,
+    DELAY_KEY,
+    DETECTION_KEY,
+    ENABLED_KEY,
+    HUMIDITY_MODE_UPDATE,
+    LIGHT_AND_VOC_MODE_UPDATE,
+    MINUTES_KEY,
+    RPM_KEY,
+    PAUSE_UPDATE,
+    TIMER_MODE_UPDATE,
+)
+
+UPDATE_NEEDED = "update_needed"
+UPDATE_DONE = "update_done"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class FetchAndUpdate:
+    def __init__(self, hass: HomeAssistant, client: FreshIntelliVent):
+        self._hass = hass
+        self._client = client
+
+        self._is_authenticated = client.sensors.authenticated
+    
+    async def update_all(self):
+        self._update_boost()
+        self._update_pause()
+        self._fetch_and_update_airing()
+        self._fetch_and_update_constant_speed()
+        self._fetch_and_update_humidity()
+        self._fetch_and_update_light_and_voc()
+        self._fetch_and_update_timer()
+
+    async def _update_boost(self):
+        boost = self._hass.data[BOOST_UPDATE]
+
+        if boost is not None and self._is_authenticated is True:
+            await self._client.update_boost(
+                enabled=boost[ENABLED_KEY],
+                rpm=boost[RPM_KEY],
+                seconds=boost[MINUTES_KEY]
+            )
+            _LOGGER.debug("Updated constant speed")
+            self._hass.data[BOOST_UPDATE] = None
+
+    async def _update_pause(self):
+        pause = self._hass.data[PAUSE_UPDATE]
+
+        if pause is not None and self._is_authenticated is True:
+            await self._client.update_pause(
+                enabled=bool(pause[ENABLED_KEY]),
+                seconds=int(pause[MINUTES_KEY]),
+            )
+            _LOGGER.debug("Updated pause: %s", pause)
+            self._hass.data[PAUSE_UPDATE] = None
+
+    async def _fetch_and_update_airing(self):
+        airing_mode = self._hass.data[AIRING_MODE_UPDATE]
+
+        if airing_mode is not None and self._is_authenticated is True:
+            await self._client.update_airing(
+                enabled=bool(airing_mode[ENABLED_KEY]),
+                minutes=int(airing_mode[MINUTES_KEY]),
+                rpm=int(airing_mode[RPM_KEY]),
+            )
+            _LOGGER.debug("Updated airing mode: %s", airing_mode)
+            self._hass.data[AIRING_MODE_UPDATE] = None
+        else:
+            self._client.fetch_airing()
+
+    async def _fetch_and_update_constant_speed(self):
+        constant_speed = self._hass.data[CONSTANT_SPEED_UPDATE]
+
+        if constant_speed is not None and self._is_authenticated is True:
+            await self._client.update_constant_speed(
+                enabled=constant_speed[ENABLED_KEY],
+                rpm=constant_speed[RPM_KEY],
+            )
+            _LOGGER.debug("Updated constant speed: %s", constant_speed)
+            self._hass.data[CONSTANT_SPEED_UPDATE] = None
+        else:
+            self._client.fetch_constant_speed()
+
+    async def _fetch_and_update_humidity(self):
+        humidity_mode = self._hass.data[HUMIDITY_MODE_UPDATE]
+
+        if humidity_mode is not None and self._is_authenticated is True:
+            await self._client.update_humidity(
+                enabled=bool(humidity_mode[ENABLED_KEY]),
+                detection=humidity_mode[DETECTION_KEY],
+                rpm=int(humidity_mode[RPM_KEY]),
+            )
+            _LOGGER.debug("Updated humidity mode: %s", humidity_mode)
+            self._hass.data[HUMIDITY_MODE_UPDATE] = None
+        else:
+            await self._client.fetch_humidity()
+
+    async def _fetch_and_update_light_and_voc(self):
+        light_and_voc_mode = self._hass.data[LIGHT_AND_VOC_MODE_UPDATE]
+
+        if light_and_voc_mode is not None and self._is_authenticated is True:
+            light = light_and_voc_mode["light"]
+            voc = light_and_voc_mode["voc"]
+
+            await self._client.update_light_and_voc(
+                light_enabled=bool(light[ENABLED_KEY]),
+                light_detection=light[DETECTION_KEY],
+                voc_enabled=bool(voc[ENABLED_KEY]),
+                voc_detection=voc[DETECTION_KEY],
+            )
+            _LOGGER.debug("Updated light and voc mode: %s", light_and_voc_mode)
+            self._hass.data[LIGHT_AND_VOC_MODE_UPDATE] = None
+        else:
+            self._client.fetch_light_and_voc()
+
+    async def _fetch_and_update_timer(self):
+        timer_mode = self._hass.data[TIMER_MODE_UPDATE]
+
+        if timer_mode is not None and self._is_authenticated is True:
+            await self._client.update_timer(
+                minutes=timer_mode[MINUTES_KEY],
+                delay_enabled=timer_mode[DELAY_KEY][ENABLED_KEY],
+                delay_minutes=timer_mode[DELAY_KEY][MINUTES_KEY],
+                rpm=timer_mode[RPM_KEY],
+            )
+            _LOGGER.debug("Updated timer mode: %s", timer_mode)
+            self._hass.data[TIMER_MODE_UPDATE] = None
+        else:
+            self._client.fetch_timer()
